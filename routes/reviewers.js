0a344547d0b0848e0ffabc2ee227a617725e9100
Alejandro Capparelli <alejandro.capparelli@gmail.com>
2020-05-01 18:18:45 +0200

    Added pagination on reviewers list

--- routes/reviewers.js
+++ routes/reviewers.js
@@ -4,22 +4,29 @@ const genres = require('../utils/constants/genres');
 const Reviewer = require('../models/reviewer');
 
 router.get('/', async function (req, res) {
-  const { genre, format } = req.query;
+  const { genre, format, page } = req.query;
   const genreName = genre && genres.find(g => g.code === genre).name;
   const queryParams = {
     genres: genreName,
-    formats: format
+    formats: format,
   };
   const filters = Object.entries(queryParams).reduce(
     (acum, [key, value]) => [null, undefined, ''].includes(value) ? acum : { ...acum, [key]: value }, {}
   );
 
   try {
+    const resultsPerPage = 20;
+    const totalElements = await Reviewer.find(filters).count();
+    const totalPages = Math.ceil(totalElements / resultsPerPage);
     const reviewers = await Reviewer
       .find(filters)
-      .limit(20).populate('author', 'name lastName avatar country');
+      .limit(resultsPerPage)
+      .skip((page - 1) * resultsPerPage)
+      .populate('author', 'name lastName avatar country');
     res.json({
-      reviewers
+      reviewers,
+      totalElements,
+      totalPages,
     });
   } catch (error) {
     res.json(error);
