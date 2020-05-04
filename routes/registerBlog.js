9165686a20d0184451519054e5fd411782b0793e
Alejandro Capparelli <alejandro.capparelli@gmail.com>
2020-05-04 20:14:17 +0200

    Added formats and country to mailchimp list

--- routes/registerBlog.js
+++ routes/registerBlog.js
@@ -32,6 +32,14 @@ const genresMapper = [
   { name: 'thriller', code: 'THR' },
 ];
 
+const formatsMapper = [
+  { name: 'papel', code: 'F_PAPEL' },
+  { name: 'ePUB', code: 'F_EPUB' },
+  { name: 'mobi', code: 'F_MOBI' },
+  { name: 'PDF', code: 'F_PDF' },
+  { name: 'audiolibro', code: 'F_AUDIO' },
+]
+
 router.post('/', async function (req, res) {
   try {
     const {
@@ -61,6 +69,9 @@ router.post('/', async function (req, res) {
     await newReviewer.save();
     const genresForMailchimp = genresMapper.reduce((acum, curr) => (
       genres.includes(curr.name) ? { ...acum, [curr.code]: 'true' } : { ...acum, [curr.code]: 'false' }), {});
+    const formatsForMailchimp = formatsMapper.reduce((acum, curr) => (
+      formats.includes(curr.name) ? { ...acum, [curr.code]: 'true' } : { ...acum, [curr.code]: 'false' }), {});
+    const userCountry = user.country ? user.country : 'N/A';
     request
       .post(url)
       .set('Content-Type', 'application/json;charset=utf-8')
@@ -70,7 +81,9 @@ router.post('/', async function (req, res) {
         'status': 'subscribed',
         'merge_fields': {
           'FNAME': user.name,
+          'PAIS': userCountry,
           ...genresForMailchimp,
+          ...formatsForMailchimp
         }
       })
       .end(function (err, response) {
@@ -125,6 +138,9 @@ router.put('/', verifyToken(), async (req, res) => {
     };
     const genresForMailchimp = genresMapper.reduce((acum, curr) => (
       genres.includes(curr.name) ? { ...acum, [curr.code]: 'true' } : { ...acum, [curr.code]: 'false' }), {});
+    const formatsForMailchimp = formatsMapper.reduce((acum, curr) => (
+      formats.includes(curr.name) ? { ...acum, [curr.code]: 'true' } : { ...acum, [curr.code]: 'false' }), {});
+    const userCountry = user.country ? user.country : 'N/A';
     request
       .put(`${url}${user.email.toLowerCase()}`)
       .set('Content-Type', 'application/json;charset=utf-8')
@@ -134,7 +150,9 @@ router.put('/', verifyToken(), async (req, res) => {
         'status': 'subscribed',
         'merge_fields': {
           'FNAME': user.name,
+          'PAIS': userCountry,
           ...genresForMailchimp,
+          ...formatsForMailchimp,
         }
       })
       .end(function (err, response) {
